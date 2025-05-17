99c85118df3c9ffa55a50b8d9702f4468cd5ef24
Farhin28v <avfarhin28@gmail.com>
2025-05-17 15:15:55 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3)// we are using nodejs, not browser
+
+console.log(3+
+    3);      // code readability should be high
+
+console.log("Farhin")
+
+
+let name = "Farhin"
+let age = 19
+let isloggedin = true
+
+
+// all of the above and below are some kind of datatypes (primitive)
+
+//number => 2 to the power 53 (This is the range)
+//bigint => for bigger number
+//string => "prefer in double quotes"
+//bolean=> either true/false
+//null => standalone value , also an object
+//undefined => value is not assigned yet
+//symbol => used for uniqueness
+
+
+//object -- will be talked about later
+
+console.log(typeof "Farhin")
+console.table(typeof [19,true,"Farhin"])
+
+console.log(typeof null); // output=>object
+console.log(typeof undefined); // output=>undefined
