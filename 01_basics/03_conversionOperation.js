cca6c9dcf639394fbd8ded4af6c9c5a4101c11f9
Farhin28v <avfarhin28@gmail.com>
2025-05-26 15:33:29 +0000

    commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,31 +1,51 @@
-let score = "33abc"
+//let score = "33abc"
 
 
 
 	
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//onsole.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
-// NaN => not a number
+//******* */ NaN => not a number
 //"33" => 33
 //"33abc" => NaN
-// true = 1; false 0
+// true = 1; false 0******
 
 
-let isLoggedIn = 1
+//let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
-//"" => false
-//"Farhin" => true
+//*****"" => false
+//"Farhin" => true*****
 
-let someNumber = 29;
+//let someNumber = 29;
 
-let stringNumber = String(someNumber) // to convert => String, should be capital
+//let stringNumber = String(someNumber) // ****to convert => String, should be capital***
 
-console.log(stringNumber);
+//console.log(stringNumber);
+
+//************************* OPERATIONS *************************//
+
+
+//let value = 3;
+//let negValue = -value;
+
+//console.log(negValue);
+
+
+//let str1 ="Hello";
+//let str2 = " Farhin";
+//let str3 = str1 + str2;
+//console.log(str3);
+
+// *****str => string...can be added but can't be subtracted****
+
+console.log(+true);
+
+
